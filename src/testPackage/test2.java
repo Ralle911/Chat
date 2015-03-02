77bf9e0e96d874bc841665ecd00567793bca380e
Emil <emilsandgren94@hotmail.com>
2015-03-02 14:19:44 +0100

    ändring

--- src/testPackage/test2.java
+++ src/testPackage/test2.java
@@ -3,14 +3,5 @@ package testPackage;
 import javax.swing.JOptionPane;
 
 public class test2 {
-	
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Krunk");
-		writeHEJ();
-	}
-
-	public void writeHEJ() {
-		System.out.println("HEJ");
-
-	}
+	private int hej = 0;
 }
