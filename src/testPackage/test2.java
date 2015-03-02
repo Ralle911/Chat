ebfffb7792548a1d566f6c2c6b93a773bd3529a9
Emil <emilsandgren94@hotmail.com>
2015-03-02 14:17:42 +0100

    HEJSAN

--- src/testPackage/test2.java
+++ src/testPackage/test2.java
@@ -6,5 +6,11 @@ public class test2 {
 	
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Krunk");
+		writeHEJ();
+	}
+
+	public void writeHEJ() {
+		System.out.println("HEJ");
+
 	}
 }
