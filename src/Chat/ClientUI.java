e67f0c05a7b1ca349940de96bc5e9260391f7855
Erik <eriksandgren94@hotmail.com>
2015-03-04 14:23:00 +0100

    ClientUI v.1 , Ska lägga till GroupPane och lyssnare

--- src/Chat/ClientUI.java
+++ src/Chat/ClientUI.java
@@ -1,13 +1,23 @@
 package Chat;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultCaret;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 /**
  * Viewer class to handle the GUI.
@@ -16,49 +26,91 @@ import javax.swing.text.DefaultCaret;
  */
 
 public class ClientUI extends JPanel {
-	private JTextArea taChatWindow = new JTextArea();
+	private JPanel southPanel = new JPanel();
+	private JPanel eastPanel = new JPanel();
+	private JPanel eastPanelNorth = new JPanel(new GridLayout(2,1));
+	
+	private JButton btnSend = new JButton("Send");
+	private JButton btnUsers = new JButton("Users");
+	private JButton btnGroups = new JButton("Groups");
+	private JButton btnLobby = new JButton("Lobby");
+	
+	private JTextPane tpUsers = new JTextPane();
+	private JTextPane tpGroups = new JTextPane();
+	private JTextPane tpChatWindow = new JTextPane();
+	
 	private JTextField tfMessageWindow = new JTextField();
-    private JScrollPane scroll = new JScrollPane(taChatWindow);
-	private Font txtFont = new Font("Sans-Serif", Font.BOLD , 30 );
+    private JScrollPane scroll = new JScrollPane(tpChatWindow);
+    
+	private Font txtFont = new Font("Sans-Serif", Font.BOLD , 20);
+	private SimpleAttributeSet chatFont = new SimpleAttributeSet();
+	
 	private ClientController clientController;
+	
 	
 	public ClientUI(ClientController clientController) {
 		this.clientController = clientController;
-
         initGraphics();
-
 		tfMessageWindow.addActionListener(new EnterListener());
 	}
 
     public void initGraphics() {
         setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(600,400));
+        setPreferredSize(new Dimension(900,600));
 
-        taChatWindow.setFont(txtFont);
+        tpChatWindow.setFont(txtFont);
         tfMessageWindow.setFont(txtFont);
 
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        DefaultCaret caret = (DefaultCaret)taChatWindow.getCaret();
+        DefaultCaret caret = (DefaultCaret)tpChatWindow.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-        taChatWindow.setEditable(false);
-
+        tpChatWindow.setEditable(false);
+        
+        StyleConstants.setForeground(chatFont, Color.BLACK);
+        StyleConstants.setBold(chatFont, true);
         add(scroll, BorderLayout.CENTER);
-//		add(taChatWindow, BorderLayout.CENTER);
-        add(tfMessageWindow,BorderLayout.SOUTH);
+        
+        southPanel();
+        eastPanel();
     }
 
     public void focusTextField() {
         tfMessageWindow.requestFocusInWindow();
     }
+    
+    public void southPanel() {
+    	southPanel.setLayout(new BorderLayout());
+    	southPanel.add(tfMessageWindow,BorderLayout.CENTER);
+    	southPanel.setPreferredSize(new Dimension(600,50));
+    	btnSend.setPreferredSize(new Dimension(75,40));
+    	southPanel.add(btnSend,BorderLayout.EAST);
+    	add(southPanel,BorderLayout.SOUTH);
+    }
+    
+    public void eastPanel() {
+    	eastPanel.setLayout(new BorderLayout());
+    	eastPanelNorth.add(btnUsers);
+    	eastPanelNorth.add(btnGroups);
+    	eastPanel.add(eastPanelNorth,BorderLayout.NORTH);
+    	eastPanel.add(tpUsers,BorderLayout.CENTER);
+    	eastPanel.add(btnLobby,BorderLayout.SOUTH);
+    	add(eastPanel,BorderLayout.EAST);
+    }
 
     public void appendText(String message){
-        taChatWindow.append(message + "\n");
+    	StyledDocument doc = tpChatWindow.getStyledDocument();
+    	try {
+			doc.insertString(doc.getLength(), message + "\n", chatFont);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
     }
-	
+    
 	private class EnterListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0) {
-			clientController.sendMessage(tfMessageWindow.getText());
+//			clientController.sendMessage(tfMessageWindow.getText());
+			appendText(tfMessageWindow.getText());
 			tfMessageWindow.setText("");
 		}
 	}
